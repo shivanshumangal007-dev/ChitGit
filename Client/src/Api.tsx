28aaa858417c708e32b18344a9cb7ec813d254c6
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-05-25 11:51:27 +0530

    made backend url dynamic

--- Client/src/Api.tsx
+++ Client/src/Api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 	// baseURL: "http://localhost:8000",
-	baseURL: "https://chitgit-1.onrender.com",
+	baseURL: import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
