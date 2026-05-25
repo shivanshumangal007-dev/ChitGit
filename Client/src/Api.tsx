5ffb973080bd7e459beb09f35ae96fd2416658ad
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-05-25 11:32:44 +0530

    Revert "chifted to deploy backend"
    This reverts commit a3e44b54c574fe64137773a04690ae90ff581d16.

--- Client/src/Api.tsx
+++ Client/src/Api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-	// baseURL: "http://localhost:8000",
-	baseURL: "https://chitgit-1.onrender.com",
+	baseURL: "http://localhost:8000",
+	// baseURL: "https://chitgit-1.onrender.com",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
